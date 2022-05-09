c953656cf6e9fb23119eacc213117cee8e1893a2
k9 <nathanmcninch@gmail.com>
2022-05-09 07:00:40 -0700

    add starter code for assignment

---
+++ assign-starter/src/app.js
@@ -0,0 +1,40 @@
+// this is your data
+const fiveten = [
+  { 
+    id: 0,
+    img: "https://res.cloudinary.com/dhv5sotva/image/upload/v1651974610/shoes/Five_Ten_Hiangle_Pro_Competition_Climbing_Shoes_Black_FV4744_01_standard_aqirgk.jpg",
+    description: "five ten hiangle pro"
+  },
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/dhv5sotva/image/upload/v1652069382/shoes/Five_Ten_Hiangle_Pro_Tokyo_Competition_Climbing_Shoes_White_GV7144_03_standard_rwngeo.jpg",
+    description: "five ten hiangle pro, Tokyo edition"
+  },
+  {
+    id: 2,
+    img: "https://res.cloudinary.com/dhv5sotva/image/upload/v1651974611/shoes/Five_Ten_Hiangle_Climbing_Shoes_White_EE9033_01_standard_q34t4t.jpg",
+    description: "five ten hiangle"
+  },
+  {
+    id: 3,
+    img: "https://res.cloudinary.com/dhv5sotva/image/upload/v1651974278/shoes/Five_Ten_NIAD_Moccasym_Climbing_Shoes_Red_FW2853_02_standard_hover_gsanzo.jpg",
+    description: "five ten niad moccasym"
+  }
+]
+const scarpa = [
+  {
+    id: 0,
+    img: "https://res.cloudinary.com/dhv5sotva/image/upload/v1652069168/shoes/1ipps_instinct_vsr_ext_-_blkazu_2_kdemnv.jpg",
+    description: "scarapa instinct vsr"
+  },
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/dhv5sotva/image/upload/v1652069168/shoes/ipps-web_instinctcts-blackazure-ext_z0gsee.jpg",
+    description: "scarpa instinct s new"
+  },
+  {
+    id: 2,
+    img: "https://res.cloudinary.com/dhv5sotva/image/upload/v1652069168/shoes/ipps_drago_lv_ext_-_white_yg8hu0.jpg",
+    description: "scarpa drago lv"
+  }
+]
